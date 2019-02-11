6de06f5040cf81d31ab6bc643728a88a48ce942b
skaleui <skale.ui@gmail.com>
2019-02-10 18:13:41 -0800

    left nav events

---
+++ src/components/Home.vue
@@ -0,0 +1,158 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list dense>
+        <template v-for="(item, ndx) in items">
+          <v-layout
+            v-if="item.heading"
+            :key="ndx"
+            row
+            align-center
+          >
+          </v-layout>
+          <v-list-group
+            v-else-if="item.children"
+            v-model="item.model"
+            :key="item.text"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon=""
+          >
+            <v-list-tile slot="activator">
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile
+              v-for="(child, i) in item.children"
+              :key="i"
+            >
+              <v-list-tile-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ child.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
+          <v-list-tile v-else :key="item.text" v-on:click="showAction(item)">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      color="blue darken-3"
+      dark
+      app
+      fixed
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-xs-only">Glbomantics Rewards</span>
+      </v-toolbar-title>
+      <!-- <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="search"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field> -->
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>notifications</v-icon>
+      </v-btn>
+      <v-btn icon large>
+        <v-avatar size="32px" tile>
+          <img
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
+          >
+        </v-avatar>
+      </v-btn>
+      <header-actions></header-actions>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-layout>
+          <transactions :set-dialog="setDialogVal"></transactions>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <edit-transaction :showdialog="dialog" :set-dialog="setDialogVal"></edit-transaction>
+  </div>
+</template>
+
+<script>
+import Transactions from './Transactions.vue'
+import EditTransaction from './EditTransactions.vue'
+import HeaderActions from './HeaderActions.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Transactions,
+    EditTransaction,
+    HeaderActions
+  },
+  computed: {
+    isLoggedIn () {
+      return this.$store.getters.isLoggedIn
+    }
+  },
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      { icon: 'contacts', text: 'Add Transaction' },
+      { icon: 'history', text: 'Current Month' },
+      { icon: 'content_copy', text: 'Notes' },
+      { icon: 'settings', text: 'Settings' },
+      { icon: 'chat_bubble', text: 'Send feedback' },
+      { icon: 'help', text: 'Help' }
+    ]
+  }),
+  props: {
+    source: String
+  },
+  methods: {
+    showAction: function (item) {
+      if (item.text === 'Add Transaction') {
+        this.dialog = true
+        console.log('showAction::dialog', this.dialog)
+        this.setDialogVal({'show': true, item: {}})
+      }
+    },
+    setDialogVal: function (value) {
+      console.log('Home::setDialogVal', value)
+      this.dialog = value
+    }
+  },
+  mounted: function () {
+    console.log('Is user logged in ? ', this.isLoggedIn)
+    if (!this.isLoggedIn) {
+      this.$router.push({ path: '/login' })
+    }
+  }
+}
+</script>
