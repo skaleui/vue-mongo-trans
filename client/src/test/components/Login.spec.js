27836709221693a52bb49bae5ad2cca4a7039361
skaleui <skale.ui@gmail.com>
2019-02-23 10:42:54 -0800

    Refactor client code

--- client/src/test/components/Login.spec.js
+++ client/src/test/components/Login.spec.js
@@ -1,18 +1,20 @@
-import Vue from 'vue'
-import App from '../../App.vue'
-import { shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Login from '../../components/Login.vue'
-import store from '../../store'
 
-describe ('Login.vue', function () {
-  new Vue({
-    el: '#app',
-    store,
-    components: { App },
-    template: '<App/>'
-  })
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(Vuetify)
+
+const store = new Vuex.Store({
+  state: {
+    loggedIn: false
+  }
+})
+describe('Login.vue', function () {
   it('renders Login when invoked', function () {
-    const wrapper = shallowMount(Login, {store: store})
-    expect (wrapper.findAll('h2')).toHaveLength(1)
+    const wrapper = shallowMount(Login, {store, localVue})
+    expect(wrapper.findAll('h2')).toHaveLength(1)
   })
 })
